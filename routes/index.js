862f93d3366454a89aa9b7457d1bde466ce2631f
desantojohnny <johnny.santo@gmail.com>
2022-07-06 16:26:08 -0300

    atualizacao css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var LoginController = require('../controllers/LoginControllers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Livraria Martin' });
 });
+
+/* login page */
+router.get('/login', LoginController.index)
+
+
+
+
 
 module.exports = router;
