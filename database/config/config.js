84e4fa55965e6a3110209a09506281b274cc422c
YagoLisboa <yago.lisboa@live.com>
2022-07-29 18:15:04 -0300

    Update settings file

---
+++ database/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "development": {
+      "username": "root",
+      "password": '',
+      "database": "db-livraria",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
