4a8b3c296d66db88b99af956bb9540c8532854cb
YagoLisboa <yago.lisboa@live.com>
2022-07-29 18:02:00 -0300

    Update bd-livraria.sql
    Updade tables script

--- bd-livraria.sql
+++ bd-livraria.sql
@@ -2,11 +2,16 @@ DROP DATABASE IF EXISTS livraria;
 CREATE DATABASE livraria;
 USE livraria;
 
-create table livros (
+create table BOOK (
 	id INT primary key auto_increment,
-	titulo VARCHAR(200) NOT NULL,
-	quantidade_paginas INT NOT NULL,
-	autor VARCHAR(50) NOT NULL,
-	ano_lancamento VARCHAR(50) NOT NULL,
+	title VARCHAR(200) NOT NULL,
+	release_year DATE NOT NULL,
+	price DECIMAL(6,2) NOT NULL,
+	pages INT NOT NULL,
+	url_image VARCHAR(200) NOT NULL,
+	authors VARCHAR(50) NOT NULL,
+	category VARCHAR(20) NOT NULL,
+	editora VARCHAR(50) NOT NULL,
+	ISBN VARCHAR(50) NOT NULL,
 	estoque INT NOT NULL
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
